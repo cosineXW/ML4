580104f996407e2147d030c704e618149a4d2348
RennaZ <145108859+cosineXW@users.noreply.github.com>
2026-01-28 15:59:55 -0500

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-// const fileInput = document.getElementById('file-input');
-// const preview = document.getElementById('file-img');
-
-// fileInput.addEventListener('change', (e) => {
-//     // console.log('test')
-//     const file = e.target.files[0];
-//     const objectUrl = URL.createObjectURL(file);
-//     preview.src = objectUrl;
-// })  
